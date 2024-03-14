449d997a63b05cfe3a4bd459c7eb3134c571ce05
Sergei Slobodchikov <cerhclobodhikow@mail.ru>
2024-03-14 15:19:25 +0300

    fix

--- app/src/main/java/com/example/quiz/Question.kt
+++ app/src/main/java/com/example/quiz/Question.kt
@@ -3,5 +3,4 @@ package com.example.quiz
 class Question(
     val question:String,
     val answers:List<String>,
-    val feedback:List<String>,
 )
--- app/src/main/java/com/example/quiz/QuizStorage.kt
+++ app/src/main/java/com/example/quiz/QuizStorage.kt
@@ -8,11 +8,6 @@ object QuizStorage {
         Locale.En -> quizEn
     }
 
-    fun answer(quiz: Quiz, answers: List<Int>): String = quiz
-        .questions
-        .zip(answers) { question, index -> question.feedback[index] }
-        .joinToString(separator = " ")
-
     enum class Locale {
         Ru, En
     }
@@ -25,12 +20,6 @@ object QuizStorage {
                     "0",
                     "7",
                 ),
-                feedback = listOf(
-                    "Вам не нравится наш курс.",
-                    "Вас удовлетворяет наш курс.",
-                    "Вы оценили наш курс хорошо.",
-                    "Вы оценили наш курс отлично.",
-                ),
             ),
             Question(
                 question = "Как называется лейбл звукозаписи chief keef-a?",
@@ -38,24 +27,12 @@ object QuizStorage {
                     "GLO GANG",
                     "DRAIN GANG",
                 ),
-                feedback = listOf(
-                    "Вы готовы порекомендовать наш курс.",
-                    "Вы уже порекомендовали курс своим друзьям.",
-                    "Вам нужно больше времени, чтобы порекомендовать наш курс.",
-                    "Вы не готовы рекомендовать наш курс.",
-                ),
             ),
             Question(
                 question = "Настоящее имя исполнителя Blood Orange?",
                 answers = listOf(
                     "Devonté Hynes",
                     "Rakim Athelaston Mayers",
-                ),
-                feedback = listOf(
-                    "Спасибо за обратную связь!",
-                    "Спасибо за обратную связь!",
-                    "Ждем вашу обратную связь!",
-                    "Мы надеемся, что у вас пояится возможность дать нам обратную связь.",
                 ),
             ),
         )
@@ -70,12 +47,6 @@ object QuizStorage {
                     "0",
                     "7",
                 ),
-                feedback = listOf(
-                    "You have rated us F",
-                    "You have rated us C",
-                    "You have rated us B",
-                    "You have rated us A++",
-                ),
             ),
             Question(
                 question = "What is the name of the record label of chief keef-a?",
@@ -83,24 +54,12 @@ object QuizStorage {
                     "GLO GANG",
                     "DRAIN GANG",
                 ),
-                feedback = listOf(
-                    "You want to recommend our course",
-                    "You already recommend our course",
-                    "You need more time for make recommendations",
-                    "You are not ready to recommendations",
-                ),
             ),
             Question(
                 question = "The real name of the artist Blood Orange?",
                 answers = listOf(
                     "Devonté Hynes",
                     "Rakim Athelaston Mayers",
-                ),
-                feedback = listOf(
-                    "Thanks for feedback!",
-                    "Thanks for feedback!",
-                    "Waiting for your feedback!",
-                    "We hope you will have the opportunity to give us feedback",
                 ),
             ),
         )
